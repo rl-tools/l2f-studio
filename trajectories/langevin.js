f94c375ca7ed619ce63f477b32b4ae6f92fc3d02
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-04-08 17:36:57 -0400

    3D langevin oscillator

--- trajectories/langevin.js
+++ trajectories/langevin.js
@@ -36,50 +36,87 @@ export class SecondOrderLangevin extends Trajectory {
 
         this.t_array = new Array(this.N)
         this.X_array = new Array(this.N)
-        this.V_array = new Array(this.N)
-        this.A_array = new Array(this.N)
-        const dW     = new Array(this.N)
+        this.Y_array = new Array(this.N)
+        this.Z_array = new Array(this.N)
+        this.Vx_array = new Array(this.N)
+        this.Vy_array = new Array(this.N)
+        this.Vz_array = new Array(this.N)
+        this.Ax_array = new Array(this.N)
+        this.Ay_array = new Array(this.N)
+        this.Az_array = new Array(this.N)
+        const dWx = new Array(this.N)
+        const dWy = new Array(this.N)
+        const dWz = new Array(this.N)
 
-        this.X_array[0] = 0.0
-        this.V_array[0] = 0.0
+        this.X_array[0] = 0
+        this.Y_array[0] = 0
+        this.Z_array[0] = 0
+        this.Vx_array[0] = 0
+        this.Vy_array[0] = 0
+        this.Vz_array[0] = 0
 
         for (let i = 0; i < this.N; i++) {
             this.t_array[i] = i * dt
-            dW[i] = randomGaussian() * Math.sqrt(dt)
+            dWx[i] = randomGaussian() * Math.sqrt(dt)
+            dWy[i] = randomGaussian() * Math.sqrt(dt)
+            dWz[i] = randomGaussian() * Math.sqrt(dt)
         }
 
         for (let i = 1; i < this.N; i++) {
-            const X_prev = this.X_array[i - 1]
-            const V_prev = this.V_array[i - 1]
-            this.V_array[i] = V_prev + (-gamma * V_prev - omega * omega * X_prev) * dt + sigma * dW[i]
-            this.X_array[i] = X_prev + V_prev * dt
+            const Xp = this.X_array[i - 1]
+            const Yp = this.Y_array[i - 1]
+            const Zp = this.Z_array[i - 1]
+            const Vxp = this.Vx_array[i - 1]
+            const Vyp = this.Vy_array[i - 1]
+            const Vzp = this.Vz_array[i - 1]
+            this.Vx_array[i] = Vxp + (-gamma * Vxp - omega * omega * Xp) * dt + sigma * dWx[i]
+            this.Vy_array[i] = Vyp + (-gamma * Vyp - omega * omega * Yp) * dt + sigma * dWy[i]
+            this.Vz_array[i] = Vzp + (-gamma * Vzp - omega * omega * Zp) * dt + sigma * dWz[i]
+            this.X_array[i] = Xp + Vxp * dt
+            this.Y_array[i] = Yp + Vyp * dt
+            this.Z_array[i] = Zp + Vzp * dt
         }
 
-        this.V_array = emaCausal(this.V_array, alpha)
+        this.Vx_array = emaCausal(this.Vx_array, alpha)
+        this.Vy_array = emaCausal(this.Vy_array, alpha)
+        this.Vz_array = emaCausal(this.Vz_array, alpha)
 
-        let cumulative = 0.0
+        let cx = 0
+        let cy = 0
+        let cz = 0
         for (let i = 0; i < this.N; i++) {
-            cumulative += this.V_array[i]
-            this.X_array[i] = cumulative * dt
+            cx += this.Vx_array[i]
+            cy += this.Vy_array[i]
+            cz += this.Vz_array[i]
+            this.X_array[i] = cx * dt
+            this.Y_array[i] = cy * dt
+            this.Z_array[i] = cz * dt
         }
 
-        this.A_array[0] = this.V_array[0] / dt
+        this.Ax_array[0] = this.Vx_array[0] / dt
+        this.Ay_array[0] = this.Vy_array[0] / dt
+        this.Az_array[0] = this.Vz_array[0] / dt
         for (let i = 1; i < this.N; i++) {
-            this.A_array[i] = (this.V_array[i] - this.V_array[i - 1]) / dt
+            this.Ax_array[i] = (this.Vx_array[i] - this.Vx_array[i - 1]) / dt
+            this.Ay_array[i] = (this.Vy_array[i] - this.Vy_array[i - 1]) / dt
+            this.Az_array[i] = (this.Vz_array[i] - this.Vz_array[i - 1]) / dt
         }
     }
 
     evaluate(t) {
         const dt = this.parameter_values.dt
         const T  = this.parameter_values.T
-        const t_reflected = reflectTime(t, T)
-        let i = Math.floor(t_reflected / dt)
+        const tr = reflectTime(t, T)
+        let i = Math.floor(tr / dt)
         if (i < 0) i = 0
         if (i >= this.N) i = this.N - 1
-
         const x = this.X_array[i]
-        const v = this.V_array[i]
-        return [x, 0, 0, v, 0, 0]
+        const y = this.Y_array[i]
+        const z = this.Z_array[i]
+        const vx = this.Vx_array[i]
+        const vy = this.Vy_array[i]
+        const vz = this.Vz_array[i]
+        return [x, y, z, vx, vy, vz]
     }
 }
 
@@ -93,7 +130,6 @@ function randomGaussian() {
 function emaCausal(x, alpha) {
     let y = new Array(x.length)
     if (x.length === 0) return y
-    
     y[0] = x[0]
     for (let i = 1; i < x.length; i++) {
         y[i] = alpha * x[i] + (1.0 - alpha) * y[i - 1]
@@ -101,11 +137,7 @@ function emaCausal(x, alpha) {
     return y
 }
 function reflectTime(t, T) {
-    const cycle = 2 * T
-    let t_mod = ((t % cycle) + cycle) % cycle
-    if (t_mod <= T) {
-        return t_mod
-    } else {
-        return cycle - t_mod
-    }
+    const c = 2 * T
+    let m = ((t % c) + c) % c
+    return m <= T ? m : c - m
 }
