589da6c64ea2b824b99fee535669e5dd43877719
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-07-07 14:19:57 -0400

    standardizing lissajous

--- trajectories/lissajous.js
+++ trajectories/lissajous.js
@@ -4,19 +4,29 @@ export class Lissajous extends Trajectory{
         super({
             "period": {"range": [1, 15], "default": 10},
             "scale": {"range": [0, 5], "default": 0.1},
+            "A": {"range": [-1, 1], "default": 1},
+            "B": {"range": [-1, 1], "default": 0.5},
+            "a": {"range": [0, 1], "default": 1},
+            "b": {"range": [0, 1], "default": 2},
+            "delta": {"range": [0, 1], "default": 0.5},
+            "phase": {"range": [0, 1], "default": 0.5},
         })
     }
     evaluate(t){
         const scale = this.parameter_values.scale
         const duration = this.parameter_values.period
-        const A = 0.5
-        const B = 1.0
+        const A = this.parameter_values.A
+        const B = this.parameter_values.B
+        const a = this.parameter_values.a
+        const b = this.parameter_values.b
+        const delta = this.parameter_values.delta
+        const phase = this.parameter_values.phase
         const progress = t * 2 * Math.PI / duration
         const d_progress = 2 * Math.PI / duration
-        const x = scale * Math.sin(A * progress)
-        const y = scale * Math.sin(B * progress)
-        const vx = scale * Math.cos(A * progress) * A * d_progress
-        const vy = scale * Math.cos(B * progress) * B * d_progress
+        const x = scale * A * Math.sin(a * (phase*Math.PI + progress) + delta*Math.PI)
+        const y = scale * B * Math.sin(b * (phase*Math.PI + progress))
+        const vx = scale * A * Math.cos(a * (phase*Math.PI + progress) + delta*Math.PI) * a * d_progress
+        const vy = scale * B * Math.cos(b * (phase*Math.PI + progress)) * b * d_progress
         return [x, y, 0, vx, vy, 0]
     }
 }
