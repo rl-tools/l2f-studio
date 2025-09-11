198846947ab2c584ab279b2dee49dca48b1ea069
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-09-11 16:31:06 -0700

    iframe fix

--- raptor.js
+++ raptor.js
@@ -31,7 +31,7 @@ if(window.location.hostname === "raptor.rl.tools" || urlParams.get("raptor") ===
         handleMobileLayout();
         document.getElementById("raptor-project-page").style.display = "block";
         if(urlParams.get("yt") !== "false"){
-            document.getElementById("video-container-inner").innerHTML = '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/hVzdWRFTX3k?si=9jCG10p1cvKHO7bP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+            document.getElementById("video-container-inner").innerHTML = '<iframe style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://www.youtube-nocookie.com/embed/hVzdWRFTX3k?si=9jCG10p1cvKHO7bP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
         }
         else{
             document.getElementById("video-container").style.display = "none";
