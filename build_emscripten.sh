677f007f96134a9cce159e4fbdbd0f1a931cc3ce
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-10-22 22:40:21 -0700

    new mono structure path adjustment

--- build_emscripten.sh
+++ build_emscripten.sh
@@ -10,10 +10,10 @@ fi
 docker run -it --rm \
 --mount type=bind,source=$(pwd)/wasm,target=/mnt,readonly \
 --mount type=bind,source=$(pwd)/blob,target=/blob \
---mount type=bind,source=$(cd ../.. && pwd),target=/rl_tools,readonly \
+--mount type=bind,source=$(cd ../../rl-tools/ && pwd),target=/rl_tools,readonly \
 --mount type=bind,source=$(cd ../../external/json && pwd),target=/json,readonly \
 -w /mnt \
-emscripten/emsdk:4.0.4 \
+emscripten/emsdk:4.0.17 \
 emcc -std=c++17 -I /rl_tools/include -I /json/include -DEMSCRIPTEN -DRL_TOOLS_ENABLE_JSON \
 $DEBUG_FLAGS \
 -s WASM=1 --bind -s EXPORTED_RUNTIME_METHODS='["cwrap", "ccall"]' -s EXPORT_ES6=1 -s MODULARIZE=1 -o /blob/l2f-interface.js l2f.cpp
