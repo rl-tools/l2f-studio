e91953b99fe4e44e58a45adc8fb7b2f07205f684
Jonas Eschmann <jonas.eschmann@gmail.com>
2026-04-13 17:58:02 -0700

    OrientationBodyZ

--- index.js
+++ index.js
@@ -182,6 +182,14 @@ class Policy{
             }
             case obs === "OrientationRotationMatrix":
                 return full_observation.slice(3, 12)
+            case obs === "OrientationBodyZ": {
+                const q = get_state().orientation
+                return [
+                    2 * q[1] * q[3] + 2 * q[0] * q[2],
+                    2 * q[2] * q[3] - 2 * q[0] * q[1],
+                    1 - 2 * q[1] * q[1] - 2 * q[2] * q[2],
+                ]
+            }
             case obs === "LinearVelocity" && !force_trajectory_tracking :
                 return current_velocity
             case obs === "TrajectoryTrackingLinearVelocity" || (obs === "LinearVelocity" && force_trajectory_tracking) :{
